ce78674750763e05dede4957f69f66fa21584426
devilzcore <aldosilva1@live.com>
2017-07-04 14:35:03 -0300

    Launcher Simplified v1

---
+++ Launcher Simplified/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Launcher_Simplified
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label_download = new System.Windows.Forms.Label();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_minimize = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Versão Cliente:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(121)))), ((int)(((byte)(203)))));
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(714, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Jogar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 32);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(678, 23);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label_download
+            // 
+            this.label_download.AutoSize = true;
+            this.label_download.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label_download.Location = new System.Drawing.Point(574, 57);
+            this.label_download.Name = "label_download";
+            this.label_download.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label_download.Size = new System.Drawing.Size(0, 13);
+            this.label_download.TabIndex = 2;
+            // 
+            // btn_close
+            // 
+            this.btn_close.BackColor = System.Drawing.Color.Transparent;
+            this.btn_close.BackgroundImage = global::Launcher_Simplified.Properties.Resources.close1;
+            this.btn_close.FlatAppearance.BorderSize = 0;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_close.Location = new System.Drawing.Point(866, 0);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(34, 26);
+            this.btn_close.TabIndex = 3;
+            this.btn_close.UseVisualStyleBackColor = false;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            this.btn_close.MouseEnter += new System.EventHandler(this.btn_close_MouseEnter);
+            this.btn_close.MouseLeave += new System.EventHandler(this.btn_close_MouseLeave);
+            // 
+            // btn_minimize
+            // 
+            this.btn_minimize.BackColor = System.Drawing.Color.Transparent;
+            this.btn_minimize.BackgroundImage = global::Launcher_Simplified.Properties.Resources.minimize1;
+            this.btn_minimize.FlatAppearance.BorderSize = 0;
+            this.btn_minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_minimize.Location = new System.Drawing.Point(832, 0);
+            this.btn_minimize.Name = "btn_minimize";
+            this.btn_minimize.Size = new System.Drawing.Size(34, 26);
+            this.btn_minimize.TabIndex = 4;
+            this.btn_minimize.UseVisualStyleBackColor = false;
+            this.btn_minimize.Click += new System.EventHandler(this.btn_minimize_Click);
+            this.btn_minimize.MouseEnter += new System.EventHandler(this.btn_minimize_MouseEnter);
+            this.btn_minimize.MouseLeave += new System.EventHandler(this.btn_minimize_MouseLeave);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 19);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(876, 162);
+            this.webBrowser1.TabIndex = 5;
+            this.webBrowser1.Url = new System.Uri("http://UrldoSite", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(0, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 412);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.progressBar1);
+            this.panel2.Controls.Add(this.label_download);
+            this.panel2.Location = new System.Drawing.Point(12, 316);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(876, 83);
+            this.panel2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(72)))), ((int)(((byte)(72)))));
+            this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.btn_minimize);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher Simplified";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label_download;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_minimize;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Launcher Simplified/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using Ionic.Zip;
+
+
+namespace Launcher_Simplified
+{
+    public partial class Form1 : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Barra de download
+            backgroundWorker1.RunWorkerAsync();
+
+            button1.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Torna a form1 arrastável
+        private void Form1_MouseDown(object sender,
+        System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        // Botão de fechar
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_close_MouseEnter(object sender, EventArgs e)
+        {
+            btn_close.BackgroundImage = Properties.Resources.close2;
+        }
+
+        private void btn_close_MouseLeave(object sender, EventArgs e)
+        {
+            btn_close.BackgroundImage = Properties.Resources.close1;
+        }
+
+        // Botão de minimizar
+        private void btn_minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_minimize_MouseEnter(object sender, EventArgs e)
+        {
+            btn_minimize.BackgroundImage = Properties.Resources.minimize2;
+        }
+
+        private void btn_minimize_MouseLeave(object sender, EventArgs e)
+        {
+            btn_minimize.BackgroundImage = Properties.Resources.minimize1;
+        }
+
+        // Excluir arquivos
+        static bool deleteFile(string f)
+        {
+            try
+            {
+                File.Delete(f);
+                return true;
+            }
+            catch (IOException)
+            {
+                return false;
+            }
+        }
+
+        // Controlar download das atualizações (backgroundWorker1)
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Definir o diretório de atualização do servidor (.xml)
+            string Server = "http://127.0.0.1/launcher-simplified/Updates/Updates.xml/";
+
+            // Definir a raiz do aplicativo
+            string Root = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Verifica se o arquivo de versão existe
+            FileStream fs = null;
+            if (!File.Exists("version"))
+            {
+                using (fs = File.Create("version"))
+                {
+
+                }
+
+                using (StreamWriter sw = new StreamWriter("version"))
+                {
+                    sw.Write("1.0");
+                }
+            }
+
+            // checks client version
+            string lclVersion;
+            using (StreamReader reader = new StreamReader("version"))
+            {
+                lclVersion = reader.ReadLine();
+            }
+            decimal localVersion = decimal.Parse(lclVersion);
+
+
+            // Verifica a versão do cliente
+            XDocument serverXml = XDocument.Load(@Server + "Updates.xml");
+
+            // Processo de Atualização
+            foreach (XElement update in serverXml.Descendants("update"))
+            {
+                string version = update.Element("version").Value;
+                string file = update.Element("file").Value;
+
+                decimal serverVersion = decimal.Parse(version);
+
+
+                string sUrlToReadFileFrom = Server + file;
+
+                string sFilePathToWriteFileTo = Root + file;
+
+                if (serverVersion > localVersion)
+                {
+                    Uri url = new Uri(sUrlToReadFileFrom);
+                    System.Net.HttpWebRequest request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+                    System.Net.HttpWebResponse response = (System.Net.HttpWebResponse)request.GetResponse();
+                    response.Close();
+
+                    Int64 iSize = response.ContentLength;
+
+                    Int64 iRunningByteTotal = 0;
+
+                    using (System.Net.WebClient client = new System.Net.WebClient())
+                    {
+                        using (System.IO.Stream streamRemote = client.OpenRead(new Uri(sUrlToReadFileFrom)))
+                        {
+                            using (Stream streamLocal = new FileStream(sFilePathToWriteFileTo, FileMode.Create, FileAccess.Write, FileShare.None))
+                            {
+                                int iByteSize = 0;
+                                byte[] byteBuffer = new byte[iSize];
+                                while ((iByteSize = streamRemote.Read(byteBuffer, 0, byteBuffer.Length)) > 0)
+                                {
+                                    streamLocal.Write(byteBuffer, 0, iByteSize);
+                                    iRunningByteTotal += iByteSize;
+
+                                    double dIndex = (double)(iRunningByteTotal);
+                                    double dTotal = (double)byteBuffer.Length;
+                                    double dProgressPercentage = (dIndex / dTotal);
+                                    int iProgressPercentage = (int)(dProgressPercentage * 100);
+
+                                    backgroundWorker1.ReportProgress(iProgressPercentage);
+                                }
+
+                                streamLocal.Close();
+                            }
+
+                            streamRemote.Close();
+                        }
+                    }
+
+                    // Unzip
+                    using (ZipFile zip = ZipFile.Read(file))
+                    {
+                        foreach (ZipEntry zipFiles in zip)
+                        {
+                            zipFiles.Extract(Root + "", true);
+                        }
+                    }
+
+                    // Download da nova versão
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFile(Server + "version.txt", @Root + "version");
+
+                    // Deletar os arquivos zip
+                    deleteFile(file);
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label_download.Text = "Baixando Atualizações..";
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            label_download.Text = "          Cliente Atualizado!";
+        }
+
+        // Botão de jogar
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Caminho do arquivo .exe
+            System.Diagnostics.Process.Start("cstrike.exe", "\\Valve");
+            this.Close();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
